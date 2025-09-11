6458b9acfc5aa4fbd0e121159fba6c63129fcf2e
Akhil Soni <akhilsoni2611@gmail.com>
2025-09-11 07:19:43 +0530

    Leetcode 2785

---
+++ SortVowels.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class SortVowels {
+    public String sortVowels(String s) {
+        List<Character> vowels = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                vowels.add(c);
+            }
+        }
+
+        Collections.sort(vowels);
+        StringBuilder result = new StringBuilder();
+        int vowelIndex = 0;
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                result.append(vowels.get(vowelIndex));
+                vowelIndex++;
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }  
+    private boolean isVowel(char c) {
+        return "AEIOUaeiou".indexOf(c) != -1;
+    }
+}
+
+
+
