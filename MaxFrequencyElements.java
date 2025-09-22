afb94e860913c1c22c0c8529ddab801a053c07ef
Akhil Soni <akhilsoni2611@gmail.com>
2025-09-22 07:05:07 +0530

    Leetcode problem 3005

---
+++ MaxFrequencyElements.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+public class MaxFrequencyElements {
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num , 0) + 1);
+        }
+
+        int maxFreq = 0;
+        for(int freq : map.values()){
+            maxFreq =  Math.max(maxFreq, freq);
+        }
+
+        int total = 0;
+        for(int freq : map.values()){
+            if(freq == maxFreq){
+                total += freq;
+            }
+        }
+        return total ;
+    }
+}
