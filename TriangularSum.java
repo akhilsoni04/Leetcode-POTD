858930ad49d0ed055e730efa558451e5e619f249
Akhil Soni <akhilsoni2611@gmail.com>
2025-09-30 17:48:37 +0530

    Leetcode problem 2221

---
+++ TriangularSum.java
@@ -0,0 +1,14 @@
+public class TriangularSum {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        for (int len = n; len > 1; len--) {
+            for (int i = 0; i < len - 1; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+        }
+
+        return nums[0];
+    }
+}
+
+
