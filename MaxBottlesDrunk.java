affc8b72d35f46d4d9c92f7551f59dec80fd67b9
Akhil Soni <akhilsoni2611@gmail.com>
2025-10-02 12:32:50 +0530

    Leetcode problem 3100

---
+++ MaxBottlesDrunk.java
@@ -0,0 +1,14 @@
+public class MaxBottlesDrunk {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int bottleDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange; // use empties for exchange
+            numExchange++;               // cost of exchange increases
+            bottleDrunk++;               // got one more bottle
+            emptyBottles++;              // the new bottle becomes empty
+        }
+        return bottleDrunk;
+    }
+}
